be5090a9fa5e2b88796735bb001f0a70e43d2625
Camden Bohanan <cambo@nku22N0047.hh.nku.edu>
2025-02-19 13:39:25 -0500

    Cloning folder from Part1 to Part2

---
+++ PETAPP/Index.js
@@ -0,0 +1,128 @@
+const pets = [
+    {
+        breed: 'Golden Retriever',
+        name: 'Max',
+        sex: 'Male',
+        age: 5,
+        image: 'Images/Max.jpg' 
+    },
+    {
+        breed: 'Bengal Cat',
+        name: 'Luna',
+        sex: 'Female',
+        age: 3,
+        image: 'Images/Luna.jpg' 
+    },
+    {
+        breed: 'Arabian Horse',
+        name: 'Spirit',
+        sex: 'Male',
+        age: 7,
+        image: 'Images/Spirit.jpg' 
+    },
+    {
+        breed: 'Siamese Cat',
+        name: 'Milo',
+        sex: 'Male',
+        age: 2,
+        image: 'Images/Milo.jpg' 
+    },
+    {
+        breed: 'Dachshund',
+        name: 'Daisy',
+        sex: 'Female',
+        age: 4,
+        image: 'Images/Daisy.jpg' 
+    },
+    {
+        breed: 'Labrador Retriever',
+        name: 'Charlie',
+        sex: 'Male',
+        age: 6,
+        image: 'Images/Charlie.jpg' 
+    },
+    {
+        breed: 'Maine Coon',
+        name: 'Bella',
+        sex: 'Female',
+        age: 1,
+        image: 'Images/Bella.jpg' 
+    },
+    {
+        breed: 'French Bulldog',
+        name: 'Buddy',
+        sex: 'Male',
+        age: 3,
+        image: 'Images/Buddy.jpg' 
+    },
+    {
+        breed: 'Percheron Horse',
+        name: 'Titan',
+        sex: 'Male',
+        age: 8,
+        image: 'Images/Titan.jpg' 
+    },
+    {
+        breed: 'Cocker Spaniel',
+        name: 'Rex',
+        sex: 'Male',
+        age: 4,
+        image: 'Images/Rex.jpg' 
+    },
+    {
+        breed: 'Ragdoll Cat',
+        name: 'Whiskers',
+        sex: 'Female',
+        age: 2,
+        image: 'Images/Whiskers.jpg' 
+    },
+    {
+        breed: 'Poodle',
+        name: 'Lily',
+        sex: 'Female',
+        age: 3,
+        image: 'Images/Lily.jpg' 
+    },
+    {
+        name: "Barry",
+        breed: "Borzoi",
+        age: 3,
+        sex: "Male",
+        image: "Images/Barry.jpg",
+    },
+    {
+        name: "Ben",
+        breed: "Basset Hound",
+        age: 4,
+        sex: "Male",
+        image: "Images/Ben.jpg",
+    },
+    {
+        name: "Sydney",
+        breed: "Sphynx Cat",
+        age: 2,
+        sex: "Female",
+        image: "Images/Sydney.jpg",
+    },
+    {
+        name: "Trisha",
+        breed: "Toucan Bird",
+        age: 2,
+        sex: "Female",
+        image: "Images/Trisha.jpg",
+    },
+    {
+        name: "Ned",
+        breed: "Newfoundland",
+        age: 5,
+        sex: "Male",
+        image: "Images/Ned.jpg",
+    },
+    {
+        name: "Kerry",
+        breed: "Kiwi Bird",
+        age: 1,
+        sex: "Female",
+        image: "Images/Kerry.jpg.webp",
+    }
+];
